75568040764b028f7c1f1d8578e74d11596b20bc
William Mendez <w.mendez@uniandes.edu.co>
2024-01-09 19:50:37 -0500

    Add tailwind integration and update dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+        colors: {
+            "andes-yellow": "#FFD02A",
+            "gray-dark": "#2D2D2D",
+            "pink": "#E7526D",
+            "black": "#000000",
+        },
+		extend: {},
+	},
+	plugins: [],
+}
