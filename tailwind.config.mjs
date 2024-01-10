802eb33fadb8fe3673c649d1f7274cf07c23b236
William Mendez <w.mendez@uniandes.edu.co>
2024-01-10 17:49:24 -0500

    Update tailwind.config.mjs with color extensions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-        colors: {
-            "andes-yellow": "#FFD02A",
-            "gray-dark": "#2D2D2D",
-            "pink": "#E7526D",
-            "black": "#000000",
-        },
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                "andes-yellow": "#FFD02A",
+                "gray-dark": "#2D2D2D",
+                pink: "#E7526D",
+                black: "#000000",
+            },
+        }
+    },
+    plugins: [],
+};
