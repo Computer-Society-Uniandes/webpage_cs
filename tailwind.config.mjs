dc1fb8ef9111a012f121476ea5fb59f515a4a4c9
Simoncl20 <simoncalderonl2010@gmail.com>
2024-01-15 18:51:39 -0500

    Add font dependencies and update styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary" : "#FFD02A",
+				"accent" : "#E7526D",
+				"background" : "#2D2D2D"
+			},
+			fontFamily : {
+				"ubuntu" : ["Ubuntu" , "sans-serif"],
+				"source" : ['Source Sans Pro', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
