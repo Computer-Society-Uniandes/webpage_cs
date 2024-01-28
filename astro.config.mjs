e279336b729feada26eba77823b829fe04cb0c7d
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-01-28 18:11:22 -0500

    Update params to generate in upstream
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://crola1702.github.io/webpage_cs',
+    site: 'https://computer-society-uniandes.github.io/webpage_cs',
 });
