1900ac9a24a78b0a8f9cd2fa7d24a4d4a3822c1c
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-01-28 18:06:18 -0500

    Fix site pages generation
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://crola1702.github.io',
+    site: 'https://crola1702.github.io/webpage_cs',
 });
