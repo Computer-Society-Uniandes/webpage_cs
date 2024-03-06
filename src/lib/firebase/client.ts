8c3894e337e5fed415b3e5bc5f76d8d986579aae
JMarts32 <j.santosa@uniandes.edu.co>
2024-03-05 20:38:53 -0500

    Using the .env file for everything but the api-key

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAKfmbx2Aau6CkH7oVINDs0H1N-xlfoKYY",
-  authDomain: "computer-society-webpage.firebaseapp.com",
-  projectId: "computer-society-webpage",
-  storageBucket: "computer-society-webpage.appspot.com",
-  messagingSenderId: "186300038387",
-  appId: "1:186300038387:web:a5f137f4b858b8a75c8b86",
-  measurementId: "G-PRZ7J1Q5N3"
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig, "Computer-Society");
